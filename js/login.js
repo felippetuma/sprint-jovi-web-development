4ba2b3ba8299b505565431fe61811aa5400395c8
Felippe Tuma <felippetuma@outlook.com>
2026-08-27 15:25:14 -0300

    feat(login.js): adiciona localstorage para armazenar o nome do usuário

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 const loginForm = document.getElementById("loginForm");
 
+
 function verificarLogin(event) {
     // Impede de recarregar a pagina
     event.preventDefault();
@@ -9,11 +10,20 @@ function verificarLogin(event) {
     const senha = document.getElementById("password").value.trim();
 
     if (email.includes("@") && email.length > 3 && senha.length >= 6) {
-        const nome = prompt("Login bem-sucedido! Como voce gostaria de ser chamado?");
-        if (nome !== null && nome.trim().length > 0) {
-            alert(`Bem vindo ${nome.trim()}, estamos te redirecionando para outra pagina!`);
+        const userRegistered = localStorage.getItem("userRegistered");
+        const nameUser = localStorage.getItem("nameUser");
+
+        if (!userRegistered || !nameUser) {
+            const name = prompt("Login bem-sucedido! Como você gostaria de ser chamado?");
+            if(name !== null && name.trim().length > 0) {
+                localStorage.setItem("nameUser", name.trim());
+                localStorage.setItem("userRegistered", "true");
+                alert(`Bem-vindo ${name.trim()}, estamos te redirecionando para outra página!`);
+            } else {
+                alert("Bem-vindo, estamos te redirecionando para outra página!");
+            }
         } else {
-            alert("Bem vindo, estamos te redirecionando para outra pagina!");
+            alert(`Bem-vindo ${nameUser}, estamos te redirecionando para outra página!`);
         }
 
         // Redirecionar para prox pagina
@@ -22,6 +32,7 @@ function verificarLogin(event) {
         alert("Login invalido, algum dos dados inseridos estao incorretos");
     }
 }
+
 
 if (loginForm) {
     loginForm.addEventListener("submit", verificarLogin);
